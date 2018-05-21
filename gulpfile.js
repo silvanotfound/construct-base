404f3acb33ef9043751305df1e771243b965c31f
Carlos Silva <silvanotfound@gmail.com>
2018-05-21 17:51:22 -0300

    ajustes da configuração do ambiente de teste e desenvolvimento

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,30 @@
 const gulp = require('gulp');
 const jasmine = require('gulp-jasmine');
- 
-gulp.task('default', function(){
-    gulp.src('src/scripts/spec/**/*spec.js')
-    .pipe(jasmine())
+const jshint = require('gulp-jshint');
+const uglify = require('gulp-uglify');
+const clean = require('gulp-clean');
+const concat = require('gulp-concat');
+
+gulp.task('clean', function(){
+    return gulp.src('./dist/')
+            .pipe(clean());
 });
+
+gulp.task('jshint', function () {
+    return gulp.src('./src/js/**/*.js')
+            .pipe(jshint())
+            .pipe(jshint.reporter('default'));
+});
+
+gulp.task('test',['jshint'], function () {
+    return gulp.src('src/test/**/*spec.js')
+            .pipe(jasmine());
+});
+
+gulp.task('uglify',['test','clean'], function(){
+    return gulp.src('./src/js/**/*.js')
+            .pipe(concat('bundle.js'))
+            .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('default', ['uglify']);
